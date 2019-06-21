aab49b434ab881a9a013b749c3ed352d8ef6b4ef
Matrosken <piranya1399@gmail.com>
2019-06-22 01:07:28 +0400

    а

---
+++ Фалафель/Выравнивание по ширине/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ifstream inFile;
+	ofstream outFile;
+	inFile.open("txt.txt");
+	outFile.open("out.txt");
+	int n = 2;
+	int count = 0;
+	inFile.seekg(0, ios_base::beg);
+	if (inFile) {
+		char* buffer = new char[256];
+		while (!inFile.eof()){
+			inFile.seekg(count);
+			inFile.get(buffer, n);
+			outFile << buffer << "\n";
+			count += n-1;
+		}
+		inFile.close();
+		outFile.close();
+	}
+	else {
+		cout << "File not opened";
+	}
+	
+}
+
